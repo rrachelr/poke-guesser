f6fec53ad0e4c8e547dd17c9b2a779ca802a4c4b
rrachelr <rachrobins1@gmail.com>
2022-06-07 21:18:33 -0400

    Update index.js
    Added sqlite table to hold score and level information for users

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Client, DiscordAPIError } = require('discord.js');
 const { token, apikey } = require('./config.json');
+const SQLite = require("better-sqlite3");
+const sql = new SQLite("./scores.sqlite");
+
 
 const client = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
@@ -31,6 +34,15 @@ async function get_pokemon(gen){
 
 client.once('ready', () => {
     console.log('Logged in!');
+    const table = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'scores';").get();
+    if (!table['count(*)']) {
+        sql.prepare("CREATE TABLE scores (id TEXT PRIMARY KEY, user TEXT, guild TEXT, points INTEGER, level INTEGER);").run();
+        sql.prepare("CREATE UNIQUE INDEX idx_scores_id ON scores (id);").run();
+        sql.pragma("synchronous = 1");
+        sql.pragma("journal_mode = wal");
+    }
+    client.getScore = sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
+    client.setScore = sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
 });
 
 client.on('messageCreate', async message => {
